68ca012e8839faa2c98328cf80f4c6f4ee1fe45d
brunolimadev <bruno.lima.dev@outlook.com>
2020-04-16 17:15:17 -0300

    Configurando metodo post do registro

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const {check, validationResult, body } = require('express-validator')
+const bcrypt = require('bcrypt')
+
 
 /* GET home page. */
 
@@ -11,6 +14,30 @@ router.get('/', function(req, res, next) {
 router.get('/registro', function(req, res, next) {
   res.render('auth/register');
 });
+
+
+router.post('/registro', [
+
+  check('email').isEmail().withMessage('Email invalido'),
+  check('nomeCompleto').isLength({min: 3}).withMessage('Nome invalido'),
+  check('nomeUsuario').isLength({min: 3}).withMessage('Nome de usuário invalido'),
+  check('senha').isLength({min: 6}).withMessage('Senha invalida. D|eve ter no mínimo 6 caractéres'),
+
+] ,(req, res) => {
+
+
+
+  const {email, nomeCompleto, nomeUsuario, senha } = req.body
+  const senhaCripto = bcrypt.hashSync(senha, 10) 
+
+  const erro = validationResult(req)
+
+  if(erro.isEmpty())
+  {
+    console.log(erro)
+  }
+})
+
 
 router.get('/home', function(req, res, next) {
   res.render('index', { title: 'Express' });
